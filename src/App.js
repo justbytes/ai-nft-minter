b20e4f21d04d9c74db92d3728e852eba11799b29
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-03-06 21:50:14 -0800

    added inputs for ai art query and img div to display art

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Nav } from "react-bootstrap";
 
+//Import react components
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+//Import navbar
 import Navbar from "./components/Navbar";
 
 //Import styling
@@ -8,11 +11,34 @@ import "./style/App.css";
 
 function App() {
   const [account, setAccount] = useState("");
+  const [image, setImage] = useState(null);
   return (
-    <div>
+    <>
       <Navbar account={account} setAccount={setAccount} />
-      <h1>Lets make some NFTs</h1>
-    </div>
+      <div className="nft-wrapper">
+        <Form className="generate-image">
+          <Form.Group className="mb-2" controlId="formBasicNFTName">
+            <Form.Control type="input" placeholder="NFT Name" />
+            <Form.Text className="text-muted">Give your NFT a name.</Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="formBasicDescription">
+            <Form.Control type="input" placeholder="NFT Description" />
+            <Form.Text className="text-muted">
+              Enter a description for the AI NFT generator
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" className="btn generate-btn" type="submit">
+            Generate Image
+          </Button>
+          <Button variant="primary" className="btn mint-btn" type="submit">
+            Mint NFT
+          </Button>
+        </Form>
+        <div className="image">
+          <img src={image} alt="AI Generated Image" />
+        </div>
+      </div>
+    </>
   );
 }
 
