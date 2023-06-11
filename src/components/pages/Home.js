263ff1aa1c8a58d87e4cabf92a62c2daae527620
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-11 13:18:06 -0700

    added more detailed instructions of how to use app

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 // Import react-bootstrap components
-import Figure from "react-bootstrap/Figure";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import Figure from 'react-bootstrap/Figure';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 //Import NFT Genie logo image
-import profilePic from "../../images/genie.jpeg";
+import profilePic from '../../images/genie.jpeg';
 
 export function Home() {
   return (
@@ -24,10 +24,10 @@ export function Home() {
                 text-to-image generator. After following the steps below you
                 will have minted your image into an NFT that is 100% yours for
                 FREE (expect for gas fees). Then head over to the NFT
-                Marketplace{" "}
+                Marketplace{' '}
                 <a className="link" href="https://opensea.io/">
                   OpenSea
-                </a>{" "}
+                </a>{' '}
                 to list, share, and admire your creation. If you like NFT Genie
                 please join our Discord and share your creations! Currently NFTs
                 will be minted to the Goerli Test Network.
@@ -37,24 +37,45 @@ export function Home() {
                   Steps to creating a NFT with NFT Genie:
                 </span>
                 <br />
-                1. Connect your MetaMask Wallet.
+                1. Connect Metamask via MetaMask browser extension.
                 <br />
                 <br />
-                2. Create a prompt. Read{" "}
+                2. Configure your MetaMask wallet to use the Goerli Testnet and
+                get WETH from a{' '}
+                <a className="link" href="https://goerlifaucet.com/">
+                  faucet
+                </a>{' '}
+                .
+                <br />
+                <br />
+                3. Connect your Metamask wallet by clicking the connect button
+                in the top right corner.
+                <br />
+                <br />
+                4. Enter a prompt in the prompt field until you get a image you
+                like. Read{' '}
                 <a
                   className="link"
                   href="https://stable-diffusion-art.com/prompt-guide/"
                 >
                   HERE
-                </a>{" "}
+                </a>{' '}
                 if you want to know what makes a good prompt.
                 <br />
                 <br />
-                3. Create your metadata. Give the NFT a name, description, and
-                attributes.
+                5. Give the image a Name, Description, and up to six attributes.
                 <br />
                 <br />
-                4. Click 'Mint NFT' pay the gas fee and you're set!
+                6. Click mint image which will then open MetaMask which will
+                prompt you to sign the transaction.
+                <br />
+                <br />
+                7. Go to{' '}
+                <a className="link" href="https://opensea.io/">
+                  OpenSea
+                </a>{' '}
+                , connect your MetaMask wallet, navigate to your profile and you
+                will be able to view your NFT!
               </p>
             </Card.Body>
           </div>
@@ -83,29 +104,29 @@ export function Home() {
                 </Card.Title>
                 <Card.Text>
                   To use NFT Genie you will need to have the MetaMask extension
-                  installed. Please follow this{" "}
+                  installed. Please follow this{' '}
                   <a
                     className="link"
                     href="https://medium.com/bankless-dao/how-to-set-up-a-metamask-wallet-a2cc255bafe2"
                   >
                     GUIDE
-                  </a>{" "}
+                  </a>{' '}
                   if you need help setting up your MetaMask Wallet.
                   <br />
                   Then configure your wallet to utilize the Goerli Testnet and
-                  grab some fake ETH{" "}
+                  grab some fake ETH{' '}
                   <a href="https://goerlifaucet.com/" className="link">
                     HERE
                   </a>
                   <br />
                   <br />
-                  Please read the conditions of use from Stable Diffusion{" "}
+                  Please read the conditions of use from Stable Diffusion{' '}
                   <a
                     className="link"
                     href="https://huggingface.co/spaces/CompVis/stable-diffusion-license"
                   >
                     HERE
-                  </a>{" "}
+                  </a>{' '}
                   before using NFT Genie.
                 </Card.Text>
                 <Button type className="project-btn" href="#/aiNFT">
