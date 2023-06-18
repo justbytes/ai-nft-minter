2e5e89d27a3d4beb42591ef7e65d495bfe84562b
Jesse <Jesse@Jesses-Mac-mini.local>
2023-06-17 19:33:05 -0700

    reorganizing file structure

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+// require("@nomicfoundation/hardhat-toolbox");
+require('./blockchain/node_modules/@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_PRIVATE_KEY}`,
