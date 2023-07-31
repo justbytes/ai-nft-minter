bc167262c21a3e874bcbbe1372fdb504f2770b3f
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-07-30 23:12:31 -0700

    adding ssl

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const https = 'https';
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const router = require('./controllers');
+const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +20,11 @@ app.use(router);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
+
+const sslOptions = {
+  cert: fs.readFileSync('/home/ubuntu/secure/certificate.crt'),
+  key: fs.readFileSync('/home/ubuntu/secure/private.key'),
+};
 
 // Create a new instance of an Apollo server class with the GraphQL schema's typeDefs and resolvers as parameters
 const startApolloServer = async (typeDefs, resolvers) => {
@@ -31,13 +38,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // connect express app with apollo middleware
   server.applyMiddleware({ app });
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
+  const httpsServer = https.createServer(sslOptions, app);
+
+  // Start the HTTPS server on port 443
+  httpsServer.listen(443, () => {
+    console.log('Express server running on HTTPS port 443');
+    console.log(
+      `Use GraphQL at https://www.thenftgenie.co${server.graphqlPath}`
+    );
   });
 };
 
