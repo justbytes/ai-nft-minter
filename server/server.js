4c89526670f692f924f12681290ab640e8e2b239
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-07-30 23:40:49 -0700

    fixing import statment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const https = 'https';
+const https = require('https');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
