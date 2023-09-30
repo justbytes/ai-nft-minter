b4cb97698938fe6b640cd16cebbb2bc9596803a1
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-09-29 20:57:43 -0700

    attempting to a server problem #1

--- server/server.js
+++ server/server.js
@@ -37,33 +37,15 @@ const startApolloServer = async () => {
   // Connect Apollo Server to the Express app
   server.applyMiddleware({ app, path: '/graphql' });
 
-  if (process.env.ENV === 'production') {
-    const sslOptions = {
-      cert: fs.readFileSync(`${process.env.CERT}`),
-      key: fs.readFileSync(`${process.env.KEY}`),
-    };
-    const httpsServer = https.createServer(sslOptions, app);
-
-    // Start the HTTPS server on port 443
-    db.once('open', () => {
-      httpsServer.listen(443, () => {
-        console.log('Express server running on HTTPS port 443');
-        console.log(
-          `Use GraphQL at https://www.thenftgenie.co${server.graphqlPath}`
-        );
-      });
+  // Start the Express app, which includes Apollo Server middleware
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
     });
-  } else {
-    // Start the Express app, which includes Apollo Server middleware
-    db.once('open', () => {
-      app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(
-          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-        );
-      });
-    });
-  }
+  });
 };
 
 app.get('*', (req, res) => {
